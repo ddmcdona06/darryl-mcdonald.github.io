127be41340c3b487a1b254331e909a26a162550d
D McDonald <darryldmcdonald@gmail.com>
2023-03-17 01:49:29 +0000

    test

---
+++ Studies/operators.js
@@ -0,0 +1,48 @@
+/**
+ * 0: Operators: Special symbols used to perform operations on values.
+
+  1. Assignment Operators: assigns values
+    = : ex. a = b -> a = b
+    += : ex. a += b -> a = a + b 
+    -= : ex. a -= b -> a = a - b
+    *= : ex. a *= b -> a = a * b
+    /= : ex. a /= b -> a = a / b
+    %= : ex. a %= b -> a = a % b
+    **= : ex. a **= b -> a = a ** b
+    
+  2. Comparison Operators: compares two values and evaluates to a boolean
+    == : equal to -> ex. a == b
+    === : strictly equals -> ex. a === b
+    != : not equal -> ex. a != b
+    !== : strictly not equal to -> ex. a !== b
+    > : greater than -> ex. a > b
+    < : less than -> ex. a < b
+    >= : greater than but equal to -> ex. a >= b
+    <= : less than but equal to -> ex. a <= b
+    ? : ternary operator *conditional* -> a ? b : x
+    
+  3. Arithmatic Operators: 
+    + : addition
+    - : subtraction
+    * : multiplication
+    / : division
+    ** : exponent
+    % : remainder
+        
+  4. Logical Operators: conditions usually resolve to true
+    && : and -> ex. a && b (both conditions resolves to true)
+    || : or -> ex. a || b (either condition resolves to true)
+    
+
+  5. Unary Operators:
+    + : unary plus tries to convert the operand into a number
+    - : unary minus tries to convert the operand into a number and negates after
+    ~ : bitwise not -> inverts all bits in the operand and returns a number
+    ! : logical not -> !a (flips the truthiness of its operand)
+    typeof : returns a string which is the type of the operand
+    ++ : adds 1 to its operand 
+    -- : decrements 1 from its operand
+    delete : deletes a specific index of an array or specific property of an object
+    typeof: returns a string that is the type of the operand
+    void: discards a return value as an expression
+ */
