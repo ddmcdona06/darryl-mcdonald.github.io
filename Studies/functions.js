9522bfa07e18e37f87ce2e45475c51adfdcc5d15
D McDonald <darryldmcdonald@gmail.com>
2023-03-19 19:46:32 +0000

    test

--- Studies/functions.js
+++ Studies/functions.js
@@ -41,4 +41,50 @@
     Since local variables are only recognized inside their functions, variables with the same name can be used in different functions
     Local variables are created when a function starts, and deleted when the function is completed
 
+
+    2: Scope
+    Scope determines the accessibility (visibility) of variables.
+    JavaScript has 3 types of scope:
+
+        - Block scope let and const variables declared inside a { } block cannot be accessed from outside the block
+                ex. {
+                    let x = 2;
+                } // x can NOT be used here
+
+        - Function scope  each function creates a new scope 
+          Variables defined inside a function are not accessible (visible) from outside the function
+          Variables declared with var, let and const are quite similar when declared inside a function
+                ex. function myFunction() {
+                                            let carName = "Volvo";   // Function Scope
+                                        }
+
+        - Global scope Variables declared Globally (outside any function) have Global Scope
+          Global variables can be accessed from anywhere in a JavaScript program
+          Variables declared with var, let and const are quite similar when declared outside a block
+                ex. var x = 2;       // Global scope
+
+
+    3: Closure
+
+    A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). 
+    In other words, a closure gives you access to an outer function's scope from an inner function. 
+    In JavaScript, closures are created every time a function is created, at function creation time.
+
+
+Examples:
  */
+
+let x = myFunction(4, 3);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
+
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+//call function
+console.log(myFunction(5,5)); // prints 25
+
+
