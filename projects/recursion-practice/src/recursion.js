573cefb39e0a12184162e084ef09d9b8f54f0863
D McDonald <darryldmcdonald@gmail.com>
2023-03-21 21:01:36 +0000

    test

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -89,6 +89,21 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+    //base
+    if(n === 1){
+      return true;
+    }
+    if(n <= 0){
+      return false;
+    }
+    if(n === 2){
+      return true;
+    }
+    //recursion
+    if(n % 2 === 1){
+      return false;
+    }
+    return powerOfTwo(n/2) 
 };
 
 // 9. Write a function that accepts a string a reverses it.
